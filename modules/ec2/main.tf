e7de770f41df5980b3543546ddbd5bda5938dee2
LearnDevOps <revnaweji@gmail.com>
2024-06-06 23:26:50 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,10 @@
 
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name = "t2s-bank-web"
+  }
+}
