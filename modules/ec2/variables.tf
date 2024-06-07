2edaac4d2f0d8b277f58446983e7648f6aff90ab
LearnDevOps <revnaweji@gmail.com>
2024-06-06 23:27:13 -0500

    Add files via upload

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
