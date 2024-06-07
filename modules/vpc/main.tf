a633bc260e6da51fd15e3b82537643868ea74ea0
LearnDevOps <revnaweji@gmail.com>
2024-06-06 23:29:08 -0500

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = "t2s-bank-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  count = length(var.subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "t2s-bank-subnet-${count.index}"
+  }
+}
 
