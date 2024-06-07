5415a3cf0ba09f9acf41ca1bacfdcad1637e22d9
LearnDevOps <revnaweji@gmail.com>
2024-06-06 23:28:22 -0500

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,1 +1,16 @@
+resource "aws_s3_bucket" "website" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "t2s-bank-website"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+}
 
