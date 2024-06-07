a6048f49023bb7b908c0b0f3771e0562d9a9c1e0
LearnDevOps <revnaweji@gmail.com>
2024-06-06 23:27:41 -0500

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,1 +1,16 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  storage_type         = var.storage_type
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  identifier           = var.db_identifier
+  username             = var.username
+  password             = var.password
+  parameter_group_name = var.parameter_group_name
+  skip_final_snapshot  = true
 
+  tags = {
+    Name = "t2s-bank-db"
+  }
+}
