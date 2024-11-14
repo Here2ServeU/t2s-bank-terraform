ae4fcdc47bccbec72b0729c38b45e50a4b8e2854
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-13 19:44:52 -0600

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "t2s-bank-terraform-state"
+    bucket         = "t2s-bank-tf-state"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
